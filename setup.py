1030f151cce8ea80ea410a6a08789d39b5df3cc2
Lua Jeon <luajeon506@gmail.com>
2024-11-20 06:25:48 +0900

    check: naming

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkpoint',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
-    package_data={'checkpoint':["checkpoint/*"]},
+    package_data={'checkpoint_function':["checkpoint/*"]},
     include_package_data=True,
     description='functions for checkpointing',
     url='https://github.com/nyancat1232/checkpoint_function',
