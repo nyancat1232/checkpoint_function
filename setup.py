dbb21635b5d4881f6040b3776120424522172e20
Lua Jeon <luajeon506@gmail.com>
2024-11-20 07:14:13 +0900

    change: for testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='checkpoint',
     version='0.1.8',
-    packages=find_packages(exclude=['tests/*']),
+    packages=find_packages(exclude=['tests.py']),
     include_package_data=True,
     description='functions for checkpointing',
     url='https://github.com/nyancat1232/checkpoint_function',
