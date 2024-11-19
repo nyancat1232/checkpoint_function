71d5e98dc2051bfd0ff323e11ad0132e5b7f139f
Lua Jeon <luajeon506@gmail.com>
2024-11-20 05:26:21 +0900

    new: for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Checkpoint Function',
+    version='0.1.0',
+    packages=find_packages(),
+    description='functions for checkpointing',
+    url='https://github.com/yourusername/your-repo',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
