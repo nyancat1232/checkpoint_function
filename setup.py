0dbf2d0f44835dd6c43abe618d5b76b2f34e7c2f
Lua Jeon <luajeon506@gmail.com>
2024-11-20 06:24:13 +0900

    fix:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='checkpoint',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     package_data={'checkpoint':["checkpoint/*"]},
     include_package_data=True,
