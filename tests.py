4a733e8fef9dfa5d652bc46ae170e82c680ff4ac
Lua Jeon <luajeon506@gmail.com>
2024-11-27 01:34:12 +0900

    add: testcase

--- tests.py
+++ tests.py
@@ -9,6 +9,15 @@ class TestExample(unittest.TestCase):
             x = yield val,'checkpoint1'
             yield val+x,'checkpoint2'
         self.assertEqual(func(3).checkpoint2(checkpoint1=2),5)
+    def test_iteration(self):
+        @cp.CheckPointFunctionDecoration
+        def func(val:int):
+            yield val,'checkpoint1'
+            yield val+1,'checkpoint2'
+            yield val-10,'checkpoint3'
+        
+        self.assertListEqual([3,4,-7],list(func(3).checkpoint3))
+ 
 
 if __name__ == '__main__':
     unittest.main()
